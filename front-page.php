9dc306bdc24a45362bc1514b8f404d926f6fd6e4
MilesAngels <mangels@alphaworks.tech>
2022-06-09 15:47:56 -0700

    front-page

--- front-page.php
+++ front-page.php
@@ -10,9 +10,9 @@
 
  ?>
 
- <section class="">
+ <section class="author-info">
      <h2>Miles Marie Angels</h2>
-     <img src="<?php echo 'http://milesangels.local/wp-content/uploads/2022/06/pexels-alex-bargain-3448835-scaled.jpg'?>">
+     <img class="profile-image" src="<?php echo 'http://milesangels.local/wp-content/uploads/2022/06/pexels-alex-bargain-3448835-scaled.jpg'?>">
      <p>Hey, I'm <strong>Miles</strong>.</p>
      <p>I am a <strong>Wordpress</strong> developer.</p>
  </section>
